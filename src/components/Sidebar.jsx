83c494c2be64a3f6f4498502627353b825df6070
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-23 22:22:58 +0200

    Update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,80 @@
-export default function Sidebar() {
-  return <h1>Sidebar here</h1>
+import { NavLink, Link } from "react-router"
+
+export default function Sidebar({ ...rest }) {
+  const normalClass =
+    "rounded-lg text-white py-4 block px-8 text-xl font-medium duration-300 hover:bg-green-700"
+  return (
+    <div {...rest}>
+      <Link to="/">
+        <p className="mb-20 text-center text-4xl font-light">
+          <span className="font-black">SCP</span>Bank
+        </p>
+      </Link>
+
+      <nav>
+        <ul className="space-y-10">
+          <li>
+            <NavLink
+              to="savings"
+              className={({ isActive }) =>
+                isActive ? `bg-selectedTab ${normalClass}` : normalClass
+              }
+            >
+              Savings
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="interest"
+              className={({ isActive }) =>
+                isActive ? `bg-selectedTab ${normalClass}` : normalClass
+              }
+            >
+              Interest Calculator
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="budget"
+              className={({ isActive }) =>
+                isActive ? `bg-selectedTab ${normalClass}` : normalClass
+              }
+            >
+              Budget Planner
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="retire"
+              className={({ isActive }) =>
+                isActive ? `bg-selectedTab ${normalClass}` : normalClass
+              }
+            >
+              Retirement Planner
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="stock"
+              className={({ isActive }) =>
+                isActive ? `bg-selectedTab ${normalClass}` : normalClass
+              }
+            >
+              Stock Market
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="investment"
+              className={({ isActive }) =>
+                isActive ? `bg-selectedTab ${normalClass}` : normalClass
+              }
+            >
+              Investment Risk
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router"
+import Header from "../components/Header"
+import Sidebar from "../components/Sidebar"
 
 export default function MainLayout() {
   return (
-    <div>
-      <h1>Main Layout</h1>
+    <div className="bg-backgroundColor grid h-screen md:grid-cols-15 md:grid-rows-8">
+      <Header className="md:col-start-4 md:-col-end-1 md:px-8 md:py-5" />
+      <Sidebar className="bg-sidebarColor text-white md:col-start-1 md:col-end-4 md:row-start-1 md:-row-end-1 md:px-7 md:py-12" />
       <Outlet />
     </div>
   )
